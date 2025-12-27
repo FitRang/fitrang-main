e66bde788ec1c30e4259b3bd0fee9a1768b8b581
Noaman Ahmed <noamanahmedai@gmail.com>
2025-12-27 19:21:23 +0530

    Fixed Typo

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "Cloning FitRang repositories..."
 
 git clone https://github.com/FitRang/infrastructure
 git clone https://github.com/FitRang/profile-service
-git clone https://github.com/FitRang/indexing-wroker
+git clone https://github.com/FitRang/indexing-worker
 git clone https://github.com/FitRang/analysis_service
 git clone https://github.com/FitRang/delivery-service
 git clone https://github.com/FitRang/fanout-consumer
