fb5853d5092110ba4c0ddded6e1f8b6ff1a6b865
Noaman Ahmed <noamanahmedai@gmail.com>
2025-12-27 19:04:01 +0530

    Added all

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ git clone https://github.com/FitRang/infrastructure
 git clone https://github.com/FitRang/profile-service
 git clone https://github.com/FitRang/indexing-wroker
 git clone https://github.com/FitRang/analysis_service
+git clone https://github.com/FitRang/delivery-service
+git clone https://github.com/FitRang/fanout-consumer
+git clone https://github.com/FitRang/federation
+git clone https://github.com/FitRang/notification
 
 echo "All repositories cloned successfully"
